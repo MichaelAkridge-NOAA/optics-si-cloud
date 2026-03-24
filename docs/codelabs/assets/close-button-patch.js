46c1b4106669624215e3667872c77f285c9c8d92
Michael Akridge - NOAA <michael.akridge@noaa.gov>
2026-03-24 08:15:18 -1000

    Update close-button-patch.js

--- docs/codelabs/assets/close-button-patch.js
+++ docs/codelabs/assets/close-button-patch.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded",function(){
   var closeBtn=document.getElementById("arrow-back");
   if(closeBtn){
-    closeBtn.href="https://michaelakridge-noaa.github.io/CorAI/";
+    closeBtn.href="https://michaelakridge-noaa.github.io/optics-si-cloud/";
     closeBtn.addEventListener("click",function(e){
       e.preventDefault();
-      window.location.href="https://michaelakridge-noaa.github.io/CorAI/";
+      window.location.href="https://michaelakridge-noaa.github.io/optics-si-cloud/";
     });
   }
 });
